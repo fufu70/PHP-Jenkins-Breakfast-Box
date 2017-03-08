cc8baf4de3b1ae09b08b29117f535fc4a40f7b90
fufu70 <cmicklis@stetson.edu>
2017-03-07 21:34:17 -0500

    Added a config.xml file to be used instead of the remote file

--- install-jenkins/integration.sh
+++ install-jenkins/integration.sh
@@ -10,7 +10,19 @@ echo -n "What is your Jenkins password: "
 read -s J_PASSWORD
 echo
 
-sudo curl -L https://raw.githubusercontent.com/sebastianbergmann/php-jenkins-template/master/config.xml | \
-     java -jar jenkins-cli.jar -s http://localhost:8080 create-job php-breakfast  --username "$J_USERNAME" --password "$J_PASSWORD"
+echo -n "Do you have a git repo?[y/N]: "
+
+read HAS_GIT_REPO
+
+if [ "$HAS_GIT_REPO" == "y" ]; then
+	echo -n "HTTPS url to the repo: "
+
+	read GIT_REPO_URL
+
+	sudo sed -i "s,https://github.com/fufu70/deadbody-php,$GIT_REPO_URL,g" ~/install-jenkins/data/config.xml
+fi
+
+sudo cat ~/install-jenkins/data/config.xml | \
+     java -jar jenkins-cli.jar -s http://localhost:8080 create-job php-breakfast-job  --username "$J_USERNAME" --password "$J_PASSWORD"
 
 sudo java -jar jenkins-cli.jar -s http://localhost:8080 reload-configuration  --username "$J_USERNAME" --password "$J_PASSWORD"
